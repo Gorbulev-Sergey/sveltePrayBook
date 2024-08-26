86a96beecc6c9895f08c44804d265f718cd91a96
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-08-26 23:01:51 +0300

    1

--- src/lib/scripts/localStorage.ts
+++ src/lib/scripts/localStorage.ts
@@ -8,3 +8,21 @@ export function initialLocalStorage() {
     localStorage.getItem('lineHeight') ?? localStorage.setItem('lineHeight', JSON.stringify(1.25));
     localStorage.getItem('articleInterval') ?? localStorage.setItem('articleInterval', JSON.stringify(0.75));
 }
+
+export let isNavPanelShow = () => localStorage.getItem('isNavPanelShow');
+export let isTitlesShow = () => localStorage.getItem('isTitlesShow');
+export let bgColor = () => localStorage.getItem('bgColor');
+export let textColor = () => localStorage.getItem('textColor');
+export let fontFamily = () => localStorage.getItem('fontFamily');
+export let fontSize = () => Number(localStorage.getItem('fontSize'));
+export let lineHeight = () => Number(localStorage.getItem('lineHeight'));
+export let articleInterval = () => Number(localStorage.getItem('articleInterval'));
+
+export let setIsNavPanelShow = (v: Boolean) => localStorage.setItem('isNavPanelShow', JSON.stringify(v));
+export let setIsTitlesShow = (v: Boolean) => localStorage.setItem('isTitlesShow', JSON.stringify(v));
+export let setBgColor = (v: String) => localStorage.setItem('bgColor', JSON.stringify(v));
+export let setTextColor = (v: String) => localStorage.setItem('textColor', JSON.stringify(v));
+export let setFontFamily = (v: String) => localStorage.setItem('fontFamily', JSON.stringify(v));
+export let setFontSize = (v: Number) => localStorage.setItem('fontSize', JSON.stringify(v));
+export let setLineHeight = (v: Number) => localStorage.setItem('lineHeight', JSON.stringify(v));
+export let setArticleInterval = (v: Number) => localStorage.setItem('articleInterval', JSON.stringify(v));
